68577d8e0b760c70afa70b5816c683d438c46aa6
Unika1 <unikaprj@gmail.com>
2026-06-04 20:14:35 +0545

    Comment mode created

---
+++ src/data/models/comment.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, Types } from 'mongoose';
+
+export interface IComment extends Document {
+  trek: Types.ObjectId;
+  userName: string;
+  message: string;
+  rating: number;
+}
+
+const commentSchema = new Schema<IComment>({
+  trek: { type: Schema.Types.ObjectId, ref: 'Trek', required: true },
+  userName: { type: String, required: true },
+  message: { type: String, required: true },
+  rating: { type: Number, default: 5 }
+}, { timestamps: true });
+
+export const CommentModel = mongoose.model<IComment>('Comment', commentSchema);
